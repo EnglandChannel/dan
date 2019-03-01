540ae401f372d64496e611f0339c6331ed9a5190
Enda <hautran.gg@gmail.com>
2019-03-01 11:01:30 +0700

    - Add faker gem - Inside the seeds.rb file create methods to generate User, Category and Post records inside the development database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,42 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+   
+  def seed_categories
+    hobby = ['Arts', 'Crafts', 'Sports', 'Sciences', 'Collecting', 'Reading', 'Other']
+    study = ['Arts and Humanities', 'Physical Science and Engineering', 'Math and Logic',
+            'Computer Science', 'Data Science', 'Economics and Finance', 'Business',
+            'Social Sciences', 'Language', 'Other']
+    team = ['Study', 'Development', 'Arts and Hobby', 'Other']
+  
+    hobby.each do |name|
+      Category.create(branch: 'hobby', name: name)
+    end
+  
+    study.each do |name|
+      Category.create(branch: 'study', name: name)
+    end
+  
+    team.each do |name|
+      Category.create(branch: 'team', name: name)
+    end
+  end
+  
+  def seed_posts
+    categories = Category.all
+  
+    categories.each do |category|
+      5.times do
+        Post.create(
+          title: Faker::Lorem.sentences[0], 
+          imagelink: Faker::Lorem.sentences[0],
+          price: Faker::Lorem.sentences[0],
+          user_id: rand(1..9), 
+          category_id: category.id
+        )
+      end
+    end
+  end
+
+  seed_categories
+  seed_posts
