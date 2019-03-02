ccb39b89c9d870d322eefee4f1e4dd3d01e30a2c
Enda <hautran.gg@gmail.com>
2019-03-02 11:11:57 +0700

    Add a popup window to show a full post's content - Add bootstrap's modal component to show full post's content - Render the modal inside the home page's template - Add js to fill the modal with post's content and show it - Add CSS to style the modal

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,8 +11,8 @@
 // about supported directives.
 //
 //= require rails-ujs
-//= require activestorage
 //= require turbolinks
-//= require_tree .
 //= require jquery
+//= require_tree .
 //= require bootstrap-sprockets
+//= require_self
