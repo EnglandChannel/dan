94a6495f5c38fdaa3d8624ed6d3187f83294da27
Enda <hautran.gg@gmail.com>
2019-03-11 12:14:03 +0700

    Create a UI to create new posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,26 @@
 class PostsController < ApplicationController
+    before_action :redirect_if_not_signed_in, only: [:new]
+
+    def post_params
+        params.require(:post).permit(:content, :title, :category_id)
+                             .merge(user_id: current_user.id)
+    end
+
+    def new
+        @branch = params[:branch]
+        @categories = Category.where(branch: @branch)
+        @post = Post.new
+    end
+    
+    def create
+        @post = Post.new(post_params)
+        if @post.save 
+          redirect_to post_path(@post) 
+        else
+          redirect_to root_path
+        end
+    end
+
     def show
         @post = Post.find(params[:id])
     end
@@ -33,5 +55,6 @@ class PostsController < ApplicationController
     respond_to do |format|
         format.html
         format.js { render partial: 'posts/posts_pagination_page' }
-      end
+    end
+
 end
