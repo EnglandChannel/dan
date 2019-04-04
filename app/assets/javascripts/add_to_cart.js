d19c181e6ea00bc79d342187558642efbb8f119d
Enda <hautran.gg@gmail.com>
2019-04-04 16:29:56 +0700

    hover to view details

---
+++ app/assets/javascripts/add_to_cart.js
@@ -0,0 +1,18 @@
+/*jQuery(document).ready(function() {
+    jQuery('.product-card').hover(function() {
+        jQuery(this).find('#view_details').show();
+        jQuery(this).find('#add-to-cart').show();
+        jQuery(this).find('img').css({ 
+                                opacity: 0.3,
+                                "-webkit-transition": "all 250ms ease-out",
+                                "-moz-transition": "all 250ms ease-out",
+                                "-ms-transition": "all 250ms ease-out",
+                                "-o-transition": "all 250ms ease-out",
+                                "transition": "all 250ms ease-out"
+                                });
+    }, function() {
+        jQuery(this).find('#view_details').hide();
+        jQuery(this).find('#add-to-cart').hide();
+        jQuery(this).find('img').css({ opacity: 1 });
+    });
+});*/
