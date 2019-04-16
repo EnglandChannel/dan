2ec2dd79062dd2ca5b983b19b40a68d633a91c9e
Enda <hautran.gg@gmail.com>
2019-04-16 14:25:57 +0700

    revenue page

--- app/helpers/revenues_helper.rb
+++ app/helpers/revenues_helper.rb
@@ -1,2 +1,13 @@
 module RevenuesHelper
+    def chart_last_7_days
+        'revenues/chartkick/last_7_day'
+    end
+
+    def chart_monthly
+        'revenues/chartkick/monthly'
+    end
+
+    def chart_weekly
+        'revenues/chartkick/weekly'
+    end
 end
