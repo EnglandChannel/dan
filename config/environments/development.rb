cee507ea9207062214b669623c2c3c255634cda7
Enda <hautran.gg@gmail.com>
2019-04-24 14:59:09 +0700

    add mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,15 +37,16 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
-
-  ActionMailer::Base.smtp_settings = {
-    
-                    :address        => "smtp.gmail.com",
-                    :port           => 587,
-                    :authentication => :plain,
-                    :user_name      => ENV['gmail_username'],
-                    :password       => ENV['gmail_password'],
-                    :openssl_verify_mode  => 'none' }
+  config.action_mailer.delivery_method = :smtp
+  # SMTP settings for gmail
+  config.action_mailer.smtp_settings = {
+  :address              => "smtp.gmail.com",
+  :port                 => 587,
+  :user_name            => "davithau987654321.dh@gmail.com",
+  :password             => "betyoucantfindout",
+  :authentication       => "plain",
+  :enable_starttls_auto => true
+  }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
